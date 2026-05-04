2b4bb6b5802ab844956f13125a72b4a8914be6c7
Escorpio024 <wvjimenez024@gmail.com>
2026-05-04 15:45:12 -0500

    Add aurora-frontend to PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,18 @@ module.exports = {
       env: {
         NODE_ENV: 'production'
       }
+    },
+    {
+      name: 'aurora-frontend',
+      script: 'npm',
+      args: 'start',
+      cwd: './frontend',
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
     }
   ]
 };
