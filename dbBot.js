9ef326b6c782b552f3486fe59109c0e4f5a8eb31
Escorpio024 <wvjimenez024@gmail.com>
2026-05-06 01:03:48 -0500

    fix: forzar ruta absoluta en dbBot.js para ignorar la cache de entorno de PM2 y resolver errores de tablas de SQLite

--- dbBot.js
+++ dbBot.js
@@ -1,5 +1,8 @@
 const { PrismaClient } = require('@prisma/bot-client');
+const path = require('path');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasourceUrl: `file:${path.join(__dirname, 'prisma', 'bot.db')}`
+});
 
 module.exports = prisma;
